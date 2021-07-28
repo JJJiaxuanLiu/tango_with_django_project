ee83182d14ce6b0547033bcf7cc94be7b455f848
JJJiaxuanLiu <2516220L@student.gla.ac.uk>
2021-07-28 22:10:38 +0800

    Chapter4

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(requset):
-    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(requset, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a herf='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
